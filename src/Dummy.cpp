868fa8ec62e6c19003891755bed747bd4148863c
Janosch Machowinski <janosch.machowinski@dfki.de>
2015-02-25 11:00:13 +0100

    Initial commit

---
+++ src/Dummy.cpp
@@ -0,0 +1,11 @@
+#include "Dummy.hpp"
+
+namespace orocos_cpp_base
+{
+
+	void DummyClass::welcome()
+	{
+		std::cout << "You successfully compiled and executed DummyProject. Welcome!" << std::endl;
+	}
+
+}
---
+++ src/Dummy.hpp
@@ -0,0 +1,20 @@
+#ifndef _DUMMYPROJECT_DUMMY_HPP_
+#define _DUMMYPROJECT_DUMMY_HPP_
+
+#include <iostream>
+
+namespace orocos_cpp_base
+{
+	class DummyClass
+	{
+		public: 
+			/**
+			* Print a welcome to stdout
+			* \return nothing
+			*/
+			void welcome();
+	};
+
+} // end namespace orocos_cpp_base
+
+#endif // _DUMMYPROJECT_DUMMY_HPP_
