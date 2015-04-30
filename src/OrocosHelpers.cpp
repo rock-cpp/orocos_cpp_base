426e1f845d4d17aeea14836d4b44a8ccc31346e9
Janosch Machowinski <janosch.machowinski@dfki.de>
2015-04-30 14:57:04 +0200

    Create taskContext server for client task
    This is only needed, so that the task shows up in the nameserver
    so that the introspection can pick it up.

--- src/OrocosHelpers.cpp
+++ src/OrocosHelpers.cpp
@@ -1,7 +1,8 @@
 #include "OrocosHelpers.hpp"
 
 #include <rtt/TaskContext.hpp>
-
+#include <rtt/transports/corba/TaskContextServer.hpp>
+#include <rtt/transports/corba/CorbaDispatcher.hpp>
 static RTT::TaskContext *clientTask = NULL;
 
 RTT::TaskContext* OrocosHelpers::getClientTask()
@@ -9,6 +10,10 @@ RTT::TaskContext* OrocosHelpers::getClientTask()
     if(!clientTask)
     {
         clientTask = new RTT::TaskContext("OrocosCPP");
+        
+        RTT::corba::TaskContextServer::Create( clientTask );
+    
+        RTT::corba::CorbaDispatcher::Instance( clientTask->ports(), ORO_SCHED_OTHER, RTT::os::LowestPriority );
     }
     return clientTask;
 }
